9282ceaeead1b84937219b6dfd3d8df55e23a8ec
pumba <pumba@4800e838-710d-4324-8640-0819b5b3db75>
2009-10-22 17:04:39 +0000

    PostRequestWrapper
    git-svn-id: https://xml-lt.springloops.com/source/semanticreports/trunk@79 4800e838-710d-4324-8640-0819b5b3db75

---
+++ src/java/controller/PostRequestWrapper.java
@@ -0,0 +1,88 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controller;
+
+import frontend.controller.form.RDFForm;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import org.apache.commons.io.IOUtils;
+
+/**
+ *
+ * @author Pumba
+ */
+public class PostRequestWrapper extends HttpServletRequestWrapper
+{
+    private String queryString = null;
+    Map paramMap = new Hashtable<String, String>();
+    
+    public PostRequestWrapper(HttpServletRequest request)
+    {
+        super(request);
+        setQueryString(getRequestBody());
+        setParameterMap(processParameters(getQueryString()));
+    }
+
+    @Override
+    public String getQueryString()
+    {
+        return queryString;
+    }
+
+    private void setQueryString(String queryString)
+    {
+        this.queryString = queryString;
+    }
+
+    private String getRequestBody()
+    {
+        StringWriter writer = new StringWriter();
+        String body = null;
+        try
+        {
+            IOUtils.copy(getReader(), writer);
+            body = writer.toString();
+        } catch (IOException ex) {
+            Logger.getLogger(RDFForm.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return body;
+    }
+
+    private Map processParameters(String queryString)
+    {
+	String[] params = queryString.split("&");
+        Hashtable<String, String> map = new Hashtable<String, String>();
+
+	for (String param : params)
+	{
+	    System.out.println(param);
+	    String[] array = param.split("=");
+	    String key = ResourceMapping.urlDecode(array[0]);
+	    String value = null;
+	    if (array.length > 1) value = ResourceMapping.urlDecode(array[1]);
+            map.put(key, value);
+	}
+        return map;
+    }
+
+    @Override
+    public Map getParameterMap()
+    {
+        return paramMap;
+    }
+
+    private void setParameterMap(Map paramMap)
+    {
+        this.paramMap = paramMap;
+    }
+
+}
