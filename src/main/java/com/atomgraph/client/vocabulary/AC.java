b53431c0fd7a8c52a0d559b4f2710cd3a696ad5c
Martynas <martynas@graphity.org>
2020-02-11 12:23:18 +0100

    Fixed namespace URI in AC.java Bumped Core version to 2.1.2 Bumped version to 2.1.0-SNAPSHOT

--- src/main/java/com/atomgraph/client/vocabulary/AC.java
+++ src/main/java/com/atomgraph/client/vocabulary/AC.java
@@ -36,7 +36,7 @@ public final class AC
     private static OntModel m_model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
     
     /** <p>The namespace of the vocabulary as a string</p> */
-    public static final String NS = "http://atomgraph.com/ns/client#";
+    public static final String NS = "https://w3id.org/atomgraph/client#";
     
     /** <p>The namespace of the vocabulary as a string</p>
      *  @see #NS */
